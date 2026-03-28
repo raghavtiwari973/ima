2ed18f20986f95b5050f00acbaa0f8ef8be3894b
raghavtiwari974 <raghavtiwari972003@gmail.com>
2026-03-28 23:11:43 +0530

    new update

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -8,7 +8,7 @@ const statsData = [
   { icon: Building, label: 'Corporate', value: 2000, suffix: '+' },
   { icon: Users, label: 'Participants', value: 200000, suffix: '+' },
   { icon: UserCheck, label: 'Members', value: 2000, suffix: '+' },
-  { icon: Trophy, label: 'Awards Won', value: 18, suffix: '+' },
+  { icon: Trophy, label: 'LMA Awards Won', value: 18, suffix: '+' },
   { icon: Handshake, label: 'Partner Companies', value: 100, suffix: '+' },
   { icon: Calendar, label: 'Events Annually', value: 150, suffix: '+' },
 ];
